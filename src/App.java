9397413c3a908e72564653af6e27de453104ee2c
Claire Smith <ringmar@chalmers.se>
2022-11-07 11:14:53 +0100

    PersonB Commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Goodbye, World!");
+        System.out.println("Goodbye!!! World!");
         System.out.println("Hey hey");
         System.out.println("Yoyoyoyo");
         System.out.println("Am I actually learning anything?");
